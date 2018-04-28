75af34dae408444cfc5e8cb4f0e355b240206ea4
scYao <2247523672@qq.com>
2018-04-28 17:23:33 +0800

    database

--- polls/models.py
+++ polls/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 
 # Create your models here.
-class Quesetion(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        pass
+
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Quesetion, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
